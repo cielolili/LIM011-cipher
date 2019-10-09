c092f42d15ab42204b132d885f1c4f9dc7e51f73
cielolili <cielito.torres@hotmail.com>
2019-10-09 11:09:48 -0500

    cifrado terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,40 @@
 window.cipher = {
-  // Acá escribe tu código.
-};
+//Si necesitamos incluir varias instrucciones en nuestra función se incluyen las llaves y el retorno.
+encode: (offset, string) => {
+  let nuevoTexto = '';
+  for (let i = 0; i < string.length; i++) {
+    let convAscii = string.charCodeAt(i); //para sacarle su código ascii
+
+    if (convAscii >= 65 && convAscii <= 90) { //fromCharCode es para pasarlo a una letra
+      nuevoTexto += String.fromCharCode((convAscii - 65 + parseInt(offset)) % 26 + 65); // Mayus Ascii: 65 - 90
+    } else if (convAscii >= 97 && convAscii <= 122) {
+      nuevoTexto += String.fromCharCode((convAscii - 97 + parseInt(offset)) % 26 + 97); // Minus Ascii: 97 - 122
+    } else if (convAscii >= 48 && convAscii <= 57) { //numeros
+      nuevoTexto += String.fromCharCode((convAscii - 48 + parseInt (offset)) %10 +48); //es 10 porque empieza del 0 al 9
+    } else {
+      nuevoTexto += string[i];       // concateno los números, signos, espacios y caracteres especiales
+    }
+  }
+  return nuevoTexto; //retorno
+},
+
+
+decode: (offset, string) => {
+  let nuevoTexto = '';
+  // let key = parseInt(offset);
+  for (let i = 0; i < string.length; i++) {
+    let convAscii = string.charCodeAt(i);
+    if (convAscii >= 65 && convAscii <= 90) { //Mayus Ascii: 65-90
+      nuevoTexto += String.fromCharCode((convAscii - 90 - parseInt(offset)) % 26 + 90);
+    } else if (convAscii >= 97 && convAscii <= 122) { //Minus Ascii: 97-122
+      nuevoTexto += String.fromCharCode((convAscii - 122 - parseInt(offset)) % 26 + 122); //desde el 122 porque retrocede, empieza desde la Z
+    } else if (convAscii >= 48 && convAscii <= 57) { //numeros
+      nuevoTexto += String.fromCharCode((convAscii - 57 - parseInt(offset)) %10 + 57);
+    } else {
+      nuevoTexto += string[i]; // concateno los números, signos, espacios y caracteres especiales
+    }
+  }
+  return nuevoTexto;
+}
+
+}
---
+++ src/index.js
@@ -0,0 +1,62 @@
+// Llamo los ID "Cajas de textos y botones" que creé en mi inihtml
+let btnEncode = document.getElementById("cifrar");
+let btnDecode = document.getElementById("descifrar");
+let btnLimpiar = document.getElementById("botonLimpiar");
+let ingTexto1 = document.getElementById("nombre");
+let ingTexto2 = document.getElementById("edad");
+let ingTexto3 = document.getElementById("provincia");
+let ingTexto4 = document.getElementById("distrito");
+let ingTexto5 = document.getElementById("direccion");
+let ingTexto6 = document.getElementById("numeroDNI");
+let ingTexto7 = document.getElementById("telefono");
+let ingTexto8 = document.getElementById("pagar");
+let cajadetexto = document.getElementById("cajadetexto");
+let cajadetexto1 = document.getElementById("cajadetexto1"); //caja donde saldrá decodificado
+let texto = document.getElementById("texto"); //para codificar
+let codificado = document.getElementById("codificado"); //mandando los datos a la otra pagina
+let desplazamiento = document.getElementById ("desplazamiento");
+let desplazamiento1= document.getElementById("desplazamiento1"); //para decoodificar
+//llamo al botón Encriptar
+btnEncode.addEventListener("click", Encode);
+function Encode() {
+	let desplazamiento = document.getElementById("desplazamiento").value;
+	cajadetexto.value = cipher.encode(desplazamiento,nombre.value + ' ' + edad.value + ' ' + provincia.value + ' '+ distrito.value + ' ' + direccion.value +' '+ numeroDNI.value + ' ' + telefono.value + ' ' + pagar.value);
+ codificado.value=cipher.encode( desplazamiento,nombre.value + ' ' + edad.value + ' ' + provincia.value + ' '+ distrito.value + ' ' + direccion.value +' '+ numeroDNI.value + ' ' + telefono.value + ' ' + pagar.value);
+}
+
+
+//llamo al botón Desencriptar
+btnDecode.addEventListener("click", Decode);
+function Decode() {
+	let desplazamiento1 = document.getElementById("desplazamiento1").value; //el segundo desplazamiento
+	cajadetexto1.value = cipher.decode(desplazamiento1, texto.value); //donde quiero que me salga el resultado
+
+}
+
+
+//llamo al botón Limpiar
+btnLimpiar.addEventListener("click", Limpiar );
+function Limpiar() {
+	document.getElementById("nombre").value = "";
+	document.getElementById("desplazamiento").value = "";
+	document.getElementById("cajadetexto").value = "";
+  document.getElementById("nombre").value ="";
+  document.getElementById("edad").value ="";
+  document.getElementById("provincia").value = "";
+  document.getElementById("distrito").value = "";
+  document.getElementById("direccion").value = "";
+  document.getElementById("numeroDNI").value = "";
+  document.getElementById("telefono").value = "";
+  document.getElementById("pagar").value ="";
+  document.getElementById("cajadetexto").value ="";
+
+
+
+}
+
+// IMPORTANTE:
+// document.getElementById("desplazamiento")==> Captura la caja de texto
+// .value   ==> captura el valor de la caja de texto.
+// cajadetexto ==> es variable de salida, no tiene un valor para capturar.
+// el .value de cajadetexto ==> es igual al resultado que deseo mostrar.
+// --------------------------------------------------------
